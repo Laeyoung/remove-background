1ff53c22463b6a714e32b8995b4c4c35c7d860e5
Laeyoung Chang <gadise@gmail.com>
2020-08-31 17:38:37 +0900

    add getObjectCooridnates() api and sample images

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const detectron2Endpoint = "https://master-ainized-detectron2-gkswjdzz.endpoint.ainize.ai"
+const superagent = require('superagent')
 
-module.exports.getObjectCoordinates = function getObjectCoordinates (date = null) {
+const detectron2Endpoint = 'https://master-ainized-detectron2-gkswjdzz.endpoint.ainize.ai'
 
-
-  if (!date) date = new Date()
-
-  return Astronomy.Illumination('Mars', date).geo_dist
+async function getObjectCoordinates () {
+  return superagent
+    .post(`${detectron2Endpoint}/predictions`)
+    .accept('application/json')
+    .type('form')
+    .set({ preview: false })
+    .attach('file', './cat.jpg')
 }
 
 
-curl -X POST "https://master-ainized-detectron2-gkswjdzz.endpoint.ainize.ai/predictions" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "preview=true"
+//curl -X POST "https://master-ainized-detectron2-gkswjdzz.endpoint.ainize.ai/predictions" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "preview=true"
 
 // module.exports.getObject = function getTimeDistance (date = null) {
 //   const distance = this.getDistance(date)
@@ -17,4 +20,13 @@ curl -X POST "https://master-ainized-detectron2-gkswjdzz.endpoint.ainize.ai/pred
 //   return distance * C_PER_AU
 // }
 
+
+
+getObjectCoordinates()
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
